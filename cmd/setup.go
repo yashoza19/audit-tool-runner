d112a12c6df93124e52031e3e8f1522d8cf67fe6
Melvin Hillsman <mrhillsman@redhat.com>
2022-03-17 16:28:50 -0500

    Add minio bucket creation
    Signed-off-by: Melvin Hillsman <mrhillsman@redhat.com>

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"os"
+	"strings"
+	"time"
+)
 
 func setupCmd() *cobra.Command {
 	setup := &cobra.Command{
@@ -19,7 +28,46 @@ func validation(cmd *cobra.Command, args []string) error {
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	// Get bundles: audit-tool-orchestrator index bundles --index-image registry.redhat.io/redhat/certified-operator-index:v4.9 --container-engine podman
-	// Create bucket: https://dl.min.io/client/mc/release/linux-amd64/mc
+	log.Infof("Starting audit run %v\n", time.Now().Format("Monday 01/02/2006 15:04:05"))
+
+	// Get list of bundles from an index
+	/*exec.Command("audit-tool-orchestrator", "index", "bundles",
+	"--index-image", envy.Get("INDEX_IMAGE", "registry.redhat.io/redhat/certified-operator-index:v4.9"),
+	"--container-engine", envy.Get("CONTAINER_ENGINE", "podman"))*/
+
+	// Create bucket to store test logs
+	endpoint := os.Getenv("MINIO_ENDPOINT")
+	accessKeyID := os.Getenv("MINIO_ACCESS_KEY")
+	secretAccessKey := os.Getenv("MINIO_SECRET_ACCESS_KEY")
+	useSSL := false
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+
+	if err != nil {
+		log.Fatalf("Unable to initialize a minio client: %v\n", err)
+	}
+
+	bucket := strings.ToLower("operator-audit-" + time.Now().Format("Monday-01-02-2006-15-04-05"))
+
+	if err = minioClient.MakeBucket(context.TODO(), bucket, minio.MakeBucketOptions{}); err != nil {
+		log.Fatalf("Unable to create minio bucket to store logs: %v\n", err)
+	}
+
+	log.Infof("Created bucket %s for audit.\n", bucket)
+
+	// Create cluster pool for run
+	/*exec.Command("audit-tool-orchestrator", "orchestrate", "pool",
+	"--install-config", "sno-install-config",
+	"--name", "",
+	"--openshift", "4.9.23",
+	"--platform", "aws",
+	"--region", "us-east-1",
+	"--running", "",
+	"--size", "")*/
+
+	// Batch run
 	return nil
 }
